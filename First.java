d64722959b8a8f007c1b984927affce82e1978c9
Sul893 <lplnv456@gmail.com>
2024-11-16 11:08:59 +0300

    Test

---
+++ First.java
@@ -0,0 +1,11 @@
+public class First{
+	public static void main(String args[]){
+		int bl = 10; 
+		int cl = 20;
+		int ind =2;
+		int b2 =5;
+		int big = 20;
+
+		System.out.println((bl<<3+cl<<2)%(b2>>5/bl>>>2));
+	}
+}
